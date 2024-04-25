8bfb199f2e460e48de26e5773ebba427430fdaad
Carolina Ruiz <ing.acrd@gmail.com>
2024-04-25 04:00:52 -0400

    history of points of the current user

--- app/src/main/java/com/example/finalproject_couplepoints/AfterSignIn.java
+++ app/src/main/java/com/example/finalproject_couplepoints/AfterSignIn.java
@@ -1,15 +1,32 @@
 package com.example.finalproject_couplepoints;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AfterSignIn extends AppCompatActivity {
-
+    ActivityResultLauncher actResLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_sigin);
+        actResLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+
+                    }
+                }
+        );
+        Intent intent = new Intent(this, HistoryActivity.class);
+
+        actResLauncher.launch(intent);
     }
 }
